bec4942ea7c26343abe56114e6c433923b4de433
Robbe Titeca <robbe.titeca@gmail.com>
2025-11-04 15:37:27 +0100

    Modified proteus install script to clone from randomized cache fork

--- install-scripts/proteus.sh
+++ install-scripts/proteus.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-git clone --recurse-submodules --branch main https://github.com/proteus-core/proteus.git core
+# Use Proteus Randomized Cache Fork
+git clone --recurse-submodules --branch main https://github.com/robbelbobel/proteus-randomized-cache.git core 
+
 make -C simulation clean
 make -C simulation CORE=riscv.CoreDynamicExtMem
 make -C functional-tests CORE=riscv.CoreDynamicExtMem BUILD_CORE=0 RISCV_PREFIX=riscv64-unknown-elf
