3342199c61b8acda60fa02552a7e3cbe14295b5b
Robbe Titeca <robbe.titeca@gmail.com>
2026-02-19 16:07:56 +0100

    Modified repo to work with randomized cache proteus variation

--- install-scripts/proteus.sh
+++ install-scripts/proteus.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git clone --recurse-submodules --branch main https://github.com/proteus-core/proteus.git core
+git clone --recurse-submodules --branch main https://github.com/robbelbobel/proteus-randomized-cache core
 make -C simulation CORE=riscv.CoreExtMem32 && cp simulation/build/sim simulation/build/sim-32-i
 make -C simulation CORE=riscv.CoreDynamicExtMem32 && cp simulation/build/sim simulation/build/sim-32-o
 make -C simulation CORE=riscv.CoreExtMem64 && cp simulation/build/sim simulation/build/sim-64-i
